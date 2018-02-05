634712b482e9f8de7ff8bfef0d315470c9f88d9d
hcarneiro <hcarneiro@weboo.biz>
2018-02-05 12:33:24 +0000

    Extra check for MacOS hidden bars

--- js/menu.js
+++ js/menu.js
@@ -2,6 +2,16 @@ var $menuElement = $('[data-name="Circle"]');
 var menuInstanceId = $menuElement.data('id');
 var data = Fliplet.Widget.getData(menuInstanceId) || {};
 
+$(function () {
+  var el = $('<div style="width:100px;height:100px;overflow:scroll;position:absolute;top:-9999px;"/>'),
+    elDom = el.get(0);
+  el.appendTo('body');
+  if (elDom.offsetWidth === elDom.clientWidth) {
+    $('body').addClass("hiddenScroll");
+  }
+  el.remove();
+});
+
 $.fn.hasScrollBar = function() {
   return this.get(0).scrollHeight > this.height();
 }
@@ -20,7 +30,7 @@ $('.fl-menu-circle-header .nav-toggle').on('click', function() {
   $('.fl-menu-circle-nav-holder .nav-circle').toggleClass('active');
   $('.fl-menu-circle-nav-list-holder').toggleClass('active').scrollTop(0);
 
-  if ($('body').hasScrollBar() && $('body').hasClass('disableScroll') && Fliplet.Env.get('platform') !== 'native') {
+  if (!$('body').hasClass('hiddenScroll') && $('body').hasScrollBar() && $('body').hasClass('disableScroll') && Fliplet.Env.get('platform') !== 'native') {
     $('.fl-menu-circle-header, .fl-menu-circle-nav-holder').css({
       'right': '30px'
     });
