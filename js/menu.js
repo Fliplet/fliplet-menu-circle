67fbfd89067910fa51acecdf7e8aa1bc66696f31
Tony Wu <twu@fliplet.com>
2020-01-30 00:37:45 +0000

    Adjusts menu layout for different device types

--- js/menu.js
+++ js/menu.js
@@ -35,21 +35,11 @@ function init() {
   }
 
   $('.fl-menu-circle-header .nav-toggle').on('click', function() {
-    $('body').toggleClass('disableScroll');
+    $('body').toggleClass('disableScroll circle-menu-active');
     $(this).find('.hamburger').toggleClass('is-active');
     $(this).toggleClass('active');
     $('.fl-menu-circle-nav-holder .nav-circle').toggleClass('active');
     $('.fl-menu-circle-nav-list-holder').toggleClass('active').scrollTop(0);
-
-    if (!$('body').hasClass('hiddenScroll') && $('body').hasScrollBar() && $('body').hasClass('disableScroll') && Fliplet.Env.get('platform') !== 'native') {
-      $('.fl-menu-circle-header, .fl-menu-circle-nav-holder').css({
-        'right': '30px'
-      });
-    } else {
-      $('.fl-menu-circle-header, .fl-menu-circle-nav-holder').css({
-        'right': '15px'
-      });
-    }
   });
 
   $('[open-about-overlay]').on('click', function() {
