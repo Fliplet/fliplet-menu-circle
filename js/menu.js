ae6ed52ccb0cbc38e2a8ca35fec5fee25e6d6e04
OleksandrLavrynenko <oleksandr.lav@upplabs.com>
2021-09-23 09:59:40 +0300

    [ADD] add translation

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,8 @@
 var $menuElement = $('[data-name="Circle"]');
 var menuInstanceId = $menuElement.data('id');
 
+$($menuElement).translate();
+
 if (menuInstanceId) {
   init();
 }
@@ -8,22 +10,20 @@ if (menuInstanceId) {
 function init() {
   var data = Fliplet.Widget.getData(menuInstanceId) || {};
 
-  Fliplet.Hooks.on('addExitAppMenuLink', function () {
+  Fliplet.Hooks.on('addExitAppMenuLink', function() {
     var $exitButton = $([
       '<li class="linked with-icon" data-fl-exit-app>',
-        '<div class="fl-menu-icon">',
-          '<i class="fa fa-sign-out"></i>',
-        '</div>',
-        'Exit',
-      '</li>'
+      '<div class="fl-menu-icon">',
+      '<i class="fa fa-sign-out"></i>',
+      '</div>' + T('widgets.menuCircle.dataSource.actions.exit') + '</li>'
     ].join(''));
-  
+
     $exitButton.on('click', function onExitClick() {
       Fliplet.Navigate.exitApp();
     });
-  
+
     $menuElement.find('ul').append($exitButton);
-  
+
     // Prevent default "Exit" link from being added
     return Promise.reject();
   });
@@ -32,19 +32,22 @@ function init() {
     $('.fl-menu-circle-nav-list-holder .nav-list').addClass('with-icons');
   }
 
-  $(function () {
-    var el = $('<div style="width:100px;height:100px;overflow:scroll;position:absolute;top:-9999px;"/>'),
-      elDom = el.get(0);
+  $(function() {
+    var el = $('<div style="width:100px;height:100px;overflow:scroll;position:absolute;top:-9999px;"/>');
+    var elDom = el.get(0);
+
     el.appendTo('body');
+
     if (elDom.offsetWidth === elDom.clientWidth) {
-      $('body').addClass("hiddenScroll");
+      $('body').addClass('hiddenScroll');
     }
+
     el.remove();
   });
 
   $.fn.hasScrollBar = function() {
     return this.get(0).scrollHeight > this.height();
-  }
+  };
 
   if (data.location) {
     $('body').addClass('fl-menu-circle-left');
@@ -60,7 +63,7 @@ function init() {
     }
 
     var $toggle = $(this);
-    var $body = $('body')
+    var $body = $('body');
 
     $menuElement.find('.fl-menu-body').toggleClass('hidden');
     $toggle.find('.hamburger').toggleClass('is-active');
@@ -70,14 +73,14 @@ function init() {
       $body.toggleClass('disableScroll circle-menu-active');
       $menuElement.find('.fl-menu-circle-nav-holder .nav-circle').toggleClass('active');
       $menuElement.find('.fl-menu-circle-nav-list-holder').toggleClass('active').scrollTop(0);
-  
+
       if ($body.is(':not(.hiddenScroll).disableScroll') && $body.hasScrollBar() && Fliplet.Env.get('platform') !== 'native') {
-        $menuElement.find('.fl-menu-circle-header, .fl-menu-circle-nav-holder').css({	
-          'right': '30px'	
+        $menuElement.find('.fl-menu-circle-header, .fl-menu-circle-nav-holder').css({
+          'right': '30px'
         });
       } else {
-        $menuElement.find('.fl-menu-circle-header, .fl-menu-circle-nav-holder').css({	
-          'right': '15px'	
+        $menuElement.find('.fl-menu-circle-header, .fl-menu-circle-nav-holder').css({
+          'right': '15px'
         });
       }
     }, 0);
